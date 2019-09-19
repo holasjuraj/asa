0a5b3e3533c6237c4d02f17c6c5457bec0c69984
holasjuraj <holasjuraj@gmail.com>
2019-09-19 15:02:24 +0200

    Read_itr_pkl now adjusted to work when others are using GPU.

--- runs/read_itr_pkl.py
+++ runs/read_itr_pkl.py
@@ -1,6 +1,13 @@
+import os
 import tensorflow as tf
 import joblib
 
+# If GPUs are blocked by another user, force use specific GPU (0 or 1),
+# or run on CPU (-1).
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
 path = 'data/local/asa-test/instant-run/itr_11.pkl'
-with tf.Session().as_default():
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session(config=config).as_default():
     data = joblib.load(path)
