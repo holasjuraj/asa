6727a7941d7d0b6cb64639af872ceed8b85108a9
holasjuraj <holasjuraj@gmail.com>
2019-10-21 21:14:54 +0200

    Added gap option for resume_from_all_iters script
    resume_from_all_iters.sh:
    - added cmd option for gap (start from every n-th iteration file)
    - changed data_dir from positional argument to option

--- runs/resume_from_all_iters.sh
+++ runs/resume_from_all_iters.sh
@@ -6,20 +6,22 @@
 max_parallel=8
 
 # Check arguments
-seed='keep'
-while getopts s: option; do
+seed="keep"
+gap=1
+data_dir=""
+while getopts s:g:d: option; do
   case "${option}" in
-    s) seed=${OPTARG} shift;;
-    *) printf "usage: %s [-s seed] <data_dir>\n" $0; exit 1;;
+    g) gap=${OPTARG}; ;;
+    s) seed=${OPTARG}; ;;
+    d) data_dir=${OPTARG}; ;;
+    *) printf "usage: %s [-s seed] <data_dir>\n" $0; exit 1; ;;
   esac
-  shift
 done
 
-if [ $# -ne 1 ]; then
+if [ ! $data_dir ]; then
   printf "usage: %s [-s seed] <data_dir>\n" $0
   exit 1
 fi
-data_dir=$1
 
 
 # Make tmp dir
@@ -27,17 +29,25 @@ tmp_dir=$(date '+resumed_trainings_output-%Y_%m_%d-%H_%M')
 mkdir $tmp_dir
 
 # Launch all trainings
-i=0
+num_pids=0
+itr_i=0
 for f in $(ls -tr "$data_dir"/itr_*.pkl); do
+  # Skip $gap iterations
+  itr_i=$((itr_i+1))
+  if [ $itr_i -ne $gap ]; then
+    continue
+  fi
+  itr_i=0
+
   # Wait for batch if we reached max processes
-  if [ $i -ge $max_parallel ]; then
+  if [ $num_pids -ge $max_parallel ]; then
     sleep 1
     echo "Waiting for processes..."
     for p in ${back_pids[*]}; do
       wait $p
     done
     unset back_pids
-    i=0
+    num_pids=0
   fi
 
   # Launch another training in batch
@@ -48,8 +58,8 @@ for f in $(ls -tr "$data_dir"/itr_*.pkl); do
     ~holas3/garage/sandbox/asa/runs/asa_resume_with_new_skill.py --file $f --seed $seed &> $out && rm $out
     printf "Training from %s finished\n" $base
   ) &
-  back_pids[$i]=$!
-  i=$((i+1))
+  back_pids[$num_pids]=$!
+  num_pids=$((num_pids+1))
 
 done
 
