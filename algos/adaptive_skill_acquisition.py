83fa751deda7a8cb196ded6245c4296c25dec8a6
holasjuraj <holasjuraj@gmail.com>
2021-04-15 18:44:29 +0200

    Adapted for full-stack usage
    ASA-algo:
    - added decision for new skill
    - added integration of new skill - new hrl env, top policy
    - TF session management
    
    asa_full_stack_test.py launcher:
    - added to test full-stack deployment, almost identical to asa_test.py
    
    HierarchicalPolicy:
    - fixed skill stopping function
    
    CategoricalMLPSkillIntegrator:
    - fixed method choosing

--- algos/adaptive_skill_acquisition.py
+++ algos/adaptive_skill_acquisition.py
@@ -1,13 +1,16 @@
 import os
 import numpy as np
-
+import tensorflow as tf
 from garage.tf.algos import BatchPolopt
 from garage.tf.envs import TfEnv
+from garage.tf.policies import CategoricalMLPPolicy
 from garage.core import Serializable
 from garage.misc.overrides import overrides
 from garage.misc import logger
-from sandbox.asa.envs import SkillLearningEnv
+from garage.misc.tensor_utils import flatten_tensors, unflatten_tensors
+from sandbox.asa.envs import SkillLearningEnv, HierarchizedEnv
 from sandbox.asa.utils import PathTrie
+from sandbox.asa.policies import CategoricalMLPSkillIntegrator
 
 
 class AdaptiveSkillAcquisition(BatchPolopt):
@@ -44,18 +47,29 @@ class AdaptiveSkillAcquisition(BatchPolopt):
                          baseline=baseline,
                          **kwargs)
         self.sampler = self._top_algo.sampler
+        self._top_algo_cls = top_algo_cls
+        self._top_algo_kwargs = kwargs if kwargs is not None else dict()
         self._low_algo_cls = low_algo_cls
         self._low_algo_kwargs = low_algo_kwargs if low_algo_kwargs is not None else dict()
         self._hrl_policy = hrl_policy
+        self._last_f_score = 999999.
+        self._added_skills = 0
+        self._tf_sess = None
 
         logger.set_tensorboard_step_key('Iteration')
+
 
     @overrides
     def init_opt(self):
         return self._top_algo.init_opt()
 
+
     @overrides
     def train(self, sess=None, snapshot_mode=None):
+        if sess is None:
+            sess = tf.Session()
+            sess.__enter__()
+        self._tf_sess = sess
         if snapshot_mode is not None:
             logger.set_snapshot_mode(snapshot_mode)
         last_average_return = super(AdaptiveSkillAcquisition, self).train(sess=sess)
@@ -63,6 +77,7 @@ class AdaptiveSkillAcquisition(BatchPolopt):
             'last_average_return': last_average_return,
             'snapshot_dir': logger.get_snapshot_dir()
         }
+
 
     @overrides
     def get_itr_snapshot(self, itr, samples_data):
@@ -74,14 +89,16 @@ class AdaptiveSkillAcquisition(BatchPolopt):
         res['low_algo_kwargs'] = self._low_algo_kwargs
         return res
 
+
     @overrides
     def optimize_policy(self, itr, samples_data):
         self._top_algo.optimize_policy(itr, samples_data)
         with logger.prefix('ASA | '):
-            make_skill, start_obss, end_obss = self.decide_new_skill(samples_data)
+            make_skill, skill_subpath = self.decide_new_skill(samples_data)
             if make_skill:
-                self.make_new_skill(start_obss, end_obss)
-                # TODO! integrate skill into top-level policy
+                new_skill_pol, new_skill_id = self.create_and_train_new_skill(skill_subpath)
+                self.integrate_new_skill(new_skill_id, skill_subpath)
+
 
     def decide_new_skill(self, samples_data):
         """
@@ -91,13 +108,13 @@ class AdaptiveSkillAcquisition(BatchPolopt):
         :return: (bool: make new skill, start_obss, end_obss)
         """
         # TODO extract Trie parameters
-        min_length = 3
+        min_length = 2
         max_length = 5
-        # action_map = {0: 's', 1: 'L', 2: 'R'}  # for Minibot
-        action_map = {i: ch for i, ch in enumerate('ABCDEFGHIJKLMNO#^>v<')}  # for Gridworld
-        min_f_score = 0  # DEBUG
+        action_map = None  # {0: 's', 1: 'L', 2: 'R'}
+        min_f_score = 2
         max_results = 10
-        aggregations = ['mean']  # sublist of ['mean', 'most_freq', 'nearest_mean', 'medoid'] or 'all'
+        aggregations = []  # sublist of ['mean', 'most_freq', 'nearest_mean', 'medoid'] or 'all'
+        f_score_step_factor = 2
 
         # TODO? share path trie among more batches?
         paths = samples_data['paths']
@@ -106,56 +123,64 @@ class AdaptiveSkillAcquisition(BatchPolopt):
             actions = path['actions'].argmax(axis=1).tolist()
             observations = path['observations']
             path_trie.add_all_subpaths(
-                    actions,
-                    observations,
-                    min_length=min_length,
-                    max_length=max_length
+                actions,
+                observations,
+                min_length=min_length,
+                max_length=max_length
             )
         logger.log('Searched {} rollouts'.format(len(paths)))
 
         frequent_paths = path_trie.items(
-                action_map=action_map,
-                min_count=10,  # len(paths) * 2,   # TODO? what about this?
-                min_f_score=min_f_score,
-                max_results=max_results,
-                aggregations=aggregations
+            action_map=action_map,
+            min_count=10,  # len(paths) * 2,   # TODO? what about this?
+            min_f_score=min_f_score,
+            max_results=max_results,
+            aggregations=aggregations
         )
         logger.log('Found {} frequent paths: [index, actions, count, f-score]'.format(len(frequent_paths)))
         for i, f_path in enumerate(frequent_paths):
             logger.log('    {:2}: {:{pad}}\t{}\t{:.3f}'.format(
                 i,
-                f_path['actions_text'],
+                str(f_path['actions']),
                 f_path['count'],
                 f_path['f_score'],
-                pad=max_length))
+                pad=max_length*3))
 
-        # TODO? some more clever mechanism to decide if we need a new skill.
-        #       As-is, we take the subpath with highest f-score if it is grater then min_f_score. If no such subpath was
-        #       found, then no skill is created.
-        #       Hence Trie parameters should be max_results = 1, min_f_score = <some reasonably high number, e.g. 20>
+        # return False, None    # DEBUG prevent training of new skill
+        if self._added_skills > 0:
+            return False, None  # DEBUG add only one skill
+        # TODO? some more clever mechanism to decide if we need a new skill?
+        #       As-is, we make new skill if its f-score is more than f_score_step_factor - times greater then previous one.
         if len(frequent_paths) == 0:
-            return False, None, None
-        return False, None, None  # DEBUG prevent training of new skill
+            return False, None
         top_subpath = frequent_paths[0]
-        return True, top_subpath.start_observations, top_subpath.end_observations
+        prev_f_score, self._last_f_score = self._last_f_score, top_subpath['f_score']
+        if self._last_f_score > prev_f_score * f_score_step_factor:
+            logger.log('Decided to make new skill, since its f-score {} > {} * {}'.format(self._last_f_score, f_score_step_factor, prev_f_score))
+            logger.log('New skill is based on subpath: {}'.format(top_subpath['actions']))
+            self._added_skills += 1
+            return True, top_subpath
+        return False, None
 
-    def make_new_skill(self, start_obss, end_obss):
+
+    def create_and_train_new_skill(self, skill_subpath):
         """
-        Create and train a new skill based on given start and end observations
+        Create and train a new skill based on given subpath. The new skill policy and
+        ID are returned, and also saved in self._hrl_policy.
         """
         ## Prepare elements for training
         # Environment
         skill_learning_env = TfEnv(
                 SkillLearningEnv(
                     # base env that was wrapped in HierarchizedEnv (not fully unwrapped - may be normalized!)
-                    env=self.env.env,  # TODO! how much do we want to unwrap the environment?
-                    start_obss=start_obss,
-                    end_obss=end_obss
+                    env=self.env.env.env,  # TODO how much do we want to unwrap the environment?
+                    start_obss=skill_subpath['start_observations'],
+                    end_obss=skill_subpath['end_observations']
                 )
         )
 
         # Skill policy
-        new_skill_pol, new_skill_id = self._hrl_policy.create_new_skill(end_obss)  # blank policy to be trained
+        new_skill_pol, new_skill_id = self._hrl_policy.create_new_skill(skill_subpath['end_observations'])  # blank policy to be trained
 
         # Baseline - clone baseline specified in low_algo_kwargs, or top-algo`s baseline
         #   We need to clone baseline, as each skill policy must have its own instance
@@ -175,6 +200,8 @@ class AdaptiveSkillAcquisition(BatchPolopt):
         )
 
         # Logger parameters
+        logger.dump_tabular(with_prefix=False)
+        logger.log('Launching training of the new skill')
         logger_snapshot_dir_before = logger.get_snapshot_dir()
         logger_snapshot_mode_before = logger.get_snapshot_mode()
         logger_snapshot_gap_before = logger.get_snapshot_gap()
@@ -182,15 +209,84 @@ class AdaptiveSkillAcquisition(BatchPolopt):
                 logger_snapshot_dir_before,
                 'skill{}'.format(new_skill_id)
         ))
-        logger.set_snapshot_mode('gap')
-        logger.set_snapshot_gap(max(1, np.floor(la_kwargs['n_itr'] / 10)))
+        logger.set_snapshot_mode('none')
+        # logger.set_snapshot_gap(max(1, np.floor(la_kwargs['n_itr'] / 10)))
+        logger.push_tabular_prefix('Skill{}/'.format(new_skill_id))
         logger.set_tensorboard_step_key('Iteration')
 
         # Train new skill
         with logger.prefix('Skill {} | '.format(new_skill_id)):
-            algo.train()
+            algo.train(sess=self._tf_sess)
 
         # Restore logger parameters
+        logger.pop_tabular_prefix()
         logger.set_snapshot_dir(logger_snapshot_dir_before)
         logger.set_snapshot_mode(logger_snapshot_mode_before)
         logger.set_snapshot_gap(logger_snapshot_gap_before)
+        logger.log('Training of the new skill finished')
+
+        return new_skill_pol, new_skill_id
+
+
+    def integrate_new_skill(self, new_skill_id, new_skill_subpath):
+        # TODO? extract integrator parameter
+        skill_integration_method = CategoricalMLPSkillIntegrator.Method.SUBPATH_SKILLS_AVG
+
+        ## Hierarchized environment
+        hrl_env = HierarchizedEnv(
+                # base env that was wrapped in HierarchizedEnv (not fully unwrapped - may be normalized!)
+                env=self.env.env.env,   # TODO how much do we want to unwrap the environment?
+                num_orig_skills=self._hrl_policy.num_skills
+        )
+        tf_hrl_env = TfEnv(hrl_env)
+
+        ## Top policy
+        # 1) Get old policy from saved data
+        old_top_policy = self._hrl_policy.get_top_policy()
+
+        # 2) Get weights of old top policy
+        otp_weights = unflatten_tensors(
+                old_top_policy.get_param_values(),
+                old_top_policy.get_param_shapes()
+        )
+
+        # 3) Create weights for new top policy
+        skill_integrator = CategoricalMLPSkillIntegrator()
+        ntp_weight_values = skill_integrator.integrate_skill(
+                old_policy_weights=otp_weights,
+                method=skill_integration_method,
+                # Specific parameters for START_OBSS_SKILLS_AVG
+                subpath_start_obss=new_skill_subpath['start_observations'],
+                top_policy=old_top_policy,
+                # Specific parameters for SUBPATH_SKILLS_AVG, SUBPATH_SKILLS_SMOOTH_AVG and SUBPATH_FIRST_SKILL
+                subpath_actions=new_skill_subpath['actions']
+        )
+
+        # 4) Create new policy and randomly initialize its weights
+        new_top_policy = CategoricalMLPPolicy(
+                env_spec=tf_hrl_env.spec,  # This env counts with new skill (action space = n + 1)
+                hidden_sizes=(32, 32),     # As was in asa_test.py,
+                name='CategoricalMLPPolicyWithSkill{}'.format(new_skill_id)
+        )
+        ntp_init_op = tf.variables_initializer(new_top_policy.get_params())
+        ntp_init_op.run()
+
+        # 5) Fill new policy with adjusted weights
+        new_top_policy.set_param_values(
+                flattened_params=flatten_tensors(ntp_weight_values)
+        )
+
+        ## Adjust HRL policy and training algorithms
+        self._hrl_policy.top_policy = new_top_policy
+        hrl_env.set_hrl_policy(self._hrl_policy)
+        self.env = tf_hrl_env
+        self.policy=self._hrl_policy.get_top_policy()
+        self._top_algo = self._top_algo_cls(
+                env=tf_hrl_env,
+                policy=self._hrl_policy.get_top_policy(),
+                baseline=self.baseline,
+                **self._top_algo_kwargs
+        )
+        self.sampler = self._top_algo.sampler
+        self._top_algo.init_opt()
+        self.start_worker(self._tf_sess)
---
+++ runs/asa_full_stack_test.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+import os
+import argparse
+from datetime import datetime
+
+from sandbox.asa.algos import AdaptiveSkillAcquisition
+from sandbox.asa.envs import HierarchizedEnv
+from sandbox.asa.policies import HierarchicalPolicy
+from sandbox.asa.policies import MinibotForwardPolicy, MinibotLeftPolicy
+
+from garage.tf.algos import TRPO                     # Policy optimization algorithm
+from garage.tf.baselines import GaussianMLPBaseline  # Baseline for Advantage function { A(s, a) = Q(s, a) - B(s) }
+from sandbox.asa.envs import MinibotStepEnv          # Environment
+from garage.envs import normalize                    #
+from garage.tf.envs import TfEnv                     #
+from garage.tf.policies import CategoricalMLPPolicy, GaussianMLPPolicy  # Policy networks
+from garage.misc.instrument import run_experiment    # Experiment-running util
+
+
+## If GPUs are blocked by another user, force use specific GPU (0 or 1), or run on CPU (-1).
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Resume ASA training with new skill')
+parser.add_argument('-s', '--seed',
+                    help='specific randomization seed, "random" for random seed, '
+                         '"keep" to keep seed specified in training script. Default: "keep"',
+                    metavar='SEED', default='keep')
+args = parser.parse_args()
+
+
+# plot = True
+# if plot:
+#     # Workaround to create Qt application in main thread
+#     import matplotlib
+#     matplotlib.use('qt5Agg')
+#     import matplotlib.pyplot as plt
+#     plt.figure()
+
+
+def run_task(*_):
+
+    ## Lower level environment & policies
+    # Base (original) environment.
+    base_env = normalize(
+                MinibotStepEnv(
+                    use_maps='all',  # [0,1]
+                    discretized=True,
+                    states_cache=dict()
+                )
+    )
+    tf_base_env = TfEnv(base_env)
+
+    # Skill policies, operating in base environment
+    trained_skill_policies = [
+            MinibotForwardPolicy(env_spec=base_env.spec),
+            MinibotLeftPolicy(env_spec=base_env.spec)
+    ]
+    trained_skill_policies_stop_funcs = [
+            lambda path: len(path['actions']) >= 5,  # 5 steps to move 1 tile
+            lambda path: len(path['actions']) >= 3   # 3 steps to rotate 90°
+    ]
+    skill_policy_prototype = GaussianMLPPolicy(
+            env_spec=tf_base_env.spec,
+            hidden_sizes=(64, 64)
+    )
+
+    ## Upper level environment & policies
+    # Hierarchized environment
+    hrl_env = HierarchizedEnv(
+            env=base_env,
+            num_orig_skills=len(trained_skill_policies)
+    )
+    tf_hrl_env = TfEnv(hrl_env)
+
+    # Top policy
+    top_policy = CategoricalMLPPolicy(
+            env_spec=tf_hrl_env.spec,
+            hidden_sizes=(32, 32)
+    )
+
+    # Hierarchy of policies
+    hrl_policy = HierarchicalPolicy(
+            top_policy=top_policy,
+            skill_policy_prototype=skill_policy_prototype,
+            skill_policies=trained_skill_policies,
+            skill_stop_functions=trained_skill_policies_stop_funcs,
+            skill_max_timesteps=20
+    )
+    # Link hrl_policy and hrl_env, so that hrl_env can use skills
+    hrl_env.set_hrl_policy(hrl_policy)
+
+    ## Other
+    # Baseline
+    baseline = GaussianMLPBaseline(env_spec=tf_hrl_env.spec)
+
+    # Main ASA algorithm
+    asa_algo = AdaptiveSkillAcquisition(
+            env=tf_hrl_env,
+            hrl_policy=hrl_policy,
+            baseline=baseline,
+            top_algo_cls=TRPO,
+            low_algo_cls=TRPO,
+            # Top algo kwargs
+                batch_size=5000,
+                max_path_length=100,
+                n_itr=80,
+                discount=0.9,
+                force_batch_sampler=True,
+            low_algo_kwargs={
+                'batch_size': 2500,
+                'max_path_length': 50,
+                'n_itr': 500,
+                'discount': 0.99,
+            }
+    )
+
+    ## Launch training
+    # Configure TF session
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as session:
+        # Train HRL agent
+        asa_algo.train(sess=session)
+
+
+## Run directly
+# run_task()
+
+
+## Run pickled
+# # Erase snapshots from previous instant run
+# import shutil
+# shutil.rmtree('/home/h/holas3/garage/data/local/asa_test/instant_run', ignore_errors=False)
+
+# General experiment settings
+seed = 3                    # Will be ignored if --seed option is used
+exp_name_direct = None      # If None, exp_name will be constructed from exp_name_extra and other info. De-bug value = 'instant_run'
+exp_name_extra = 'Full_stack_Minibot'  # Name of run
+
+# Seed
+seed = seed if args.seed == 'keep' \
+       else None if args.seed == 'random' \
+       else int(args.seed)
+
+# Launch training
+run_experiment(
+        run_task,
+        # Configure TF
+        use_tf=True,
+        use_gpu=True,
+        # Name experiment
+        exp_prefix='asa-full-stack',
+        exp_name=exp_name_direct or \
+                 (datetime.now().strftime('%Y_%m_%d-%H_%M')
+                  + (('--' + exp_name_extra) if exp_name_extra else '')
+                  + (('--s' + str(seed)) if seed else '')
+                 ),
+        # Number of parallel workers for sampling
+        n_parallel=0,
+        # Snapshot information
+        snapshot_mode="all",
+        # Specifies the seed for the experiment  (random seed if None)
+        seed=seed,
+        # Plot after each batch
+        # plot=True
+)
